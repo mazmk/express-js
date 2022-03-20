2d5aa2093df323ddd8026e78956611f5e2cc650f
Faheem Sajjad <faheemwa0@gmail.com>
2022-03-20 19:45:05 +0500

    Added start command in package.json and added static index.html

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const app = express();
 const router = express.Router();
 const fs = require("fs");
 const path = require("path");
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
+
+app.use(express.static("adward"));
 
 
-app.get('/', (req, res, next) => {
-    // console.log(path(req.url));
-    console.log('baseUrl is: ',req.url);
-    // let dd = fs.readFileSync('./../')
-    res.download('mazm.zip');
-    // console.log('Request: ', req.xhr);
-    // res.render("home");
-});
+// app.get('/', (req, res, next) => {
+//     // console.log(path(req.url));
+//     console.log('baseUrl is: ',req.url);
+//     // let dd = fs.readFileSync('./../')
+//     res.download('mazm.zip');
+//     // console.log('Request: ', req.xhr);
+//     // res.render("home");
+// });
 
 // app.
 
